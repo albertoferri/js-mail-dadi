b0eaf879959c07d15adc00e7de8572c019383329
albertoferri <falberto99@gmail.com>
2024-02-01 18:09:06 +0100

    stilizzazione base con bootstrap

--- emails/js/script.js
+++ emails/js/script.js
@@ -2,6 +2,7 @@
 const emails = ['test1@gmail.com', 'test2@gmail.com', 'test3@gmail.com', 'test4@gmail.com', 'test5@gmail.com', 'test6@gmail.com', 'test7@gmail.com',];
 // creo costante per l'input dell'utente
 const userMail = prompt("Inserisci la tua email");
+// const userMail = "test1@gmail.com";
 
 // ho inserito la mail dentro l'array
 emails.push(userMail[0]);
@@ -21,9 +22,13 @@ if (findMail) {
     document.getElementById("userChoice").innerHTML = 'Email registrata!';
     console.log('Email registrata!');
 } else {
-    document.getElementById("userChoice").innerHTML = 'Email non registrata.';
+    document.getElementById("userChoice").innerHTML = 'Email non registrata!';
     console.log('Email non registrata.');
 }
 
-// console.log(emails);
+if (userMail == ""){
+    alert("attento hai inserito un campo vuoto")
+    
+}
+
 console.log(userMail)
