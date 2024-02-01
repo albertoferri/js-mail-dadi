28a6977d239deff5b4e1b42950c12b7837b0f4b4
albertoferri <falberto99@gmail.com>
2024-02-01 14:44:47 +0100

    aggiunti commenti e creato verifica dei valori usciti

--- dadi/js/script.js
+++ dadi/js/script.js
@@ -1,19 +1,28 @@
-// creo una variabile per il bottone
+// creo variabile per il bottone
 const buttonElement = document.querySelector("#button");
 
-// creoun evento al click del bottone: genero due numeri casuali
+// creo evento al click del bottone
 buttonElement.addEventListener("click",
-    function() {
-        const numberUser = Math.floor(Math.random() * 5 + 1);
-        document.getElementById("userResult").innerHTML ="Il tuo dado: " + numberUser;
-        console.log(numberUser)
-    }    
-)
-buttonElement,addEventListener("click",
-    function() {
-        const numberPc = Math.floor(Math.random() * 5 + 1);
-        document.getElementById("pcResult").innerHTML ="computer: " + numberPc;
-        console.log(numberPc)
+    function(){
+        // imposto variabili assegnate random 
+        let numberUser = Math.round(Math.random()*5+1);
+        let numberPc = Math.round(Math.random()*5+1);
+
+        document.getElementById("userResult").innerHTML = "Hai ottenuto: " + numberUser;
+        document.getElementById("pcResult").innerHTML = "Computer: " + numberPc;
+        
+        // aggiunta verifica dei valori (il più alto vince)
+        if (numberUser > numberPc){
+            document.getElementById("winner").innerHTML = "WOW HAI VINTO! SARÀ LA FORTUNA DEL PRINCIPIANTE!";
+        } else{
+            document.getElementById("winner").innerHTML = "NON HA VINTO IL COMPUTER: HAI PERSO TU!";
+        }
+        // if pareggio
+        if (numberUser == numberPc){
+            document.getElementById("winner").innerHTML = "VEDILA COSÌ, POTEVA ANDARTI PEGGIO!";
+        }
+
+        // console log
+        console.log(`Scelta utente: ${numberUser} Scelta PC: ${numberPc}`);
     }
 )
-    
